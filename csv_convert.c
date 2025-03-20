91f2f1ec4af1f6453f87e01b3dbcc3c59b3f7a38
jqn1 <154387579+jqn1@users.noreply.github.com>
2025-03-19 22:24:13 -0300

    Definir struct para almacenar orders
    Se crea la estructura donde se almacenara cada orden por separado con
    todos sus atributos desde el archivo csv
    
    Falta:
    -Hacer variable el largo de los arreglos para optimizar el uso de memoria
    -El arreglo de pizza_ingredients probablemente necesitara contener
    arreglos de caracteres dentro de si para la obtención de metricas

---
+++ csv_convert.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+
+
+    return 0;
+}
+
+
+struct Order {
+    int pizza_id;
+    int order_id;
+    char pizza_name_id[50];
+    int quantity;
+    char order_date[20];
+    char order_time[10];
+    float unit_price;
+    char pizza_size[3];
+    char pizza_category[20];
+    char pizza_ingredients[200];
+    char pizza_name[50];
+
+};
