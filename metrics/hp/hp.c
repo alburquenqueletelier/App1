9c7bd77b37c84feb4b00d7ccdd7347b81850822b
jqn1 <154387579+jqn1@users.noreply.github.com>
2025-04-05 22:09:41 -0300

    comentarios

--- metrics/hp/hp.c
+++ metrics/hp/hp.c
@@ -9,58 +9,7 @@ typedef struct Category {
 
 } Category;
 
-
-// 
-void quantities_fill(Order *orders, int orders_size, int categories_size, Category *categories){
-    for(int i = 0; i < categories_size; i++){
-        for(int j = 0; j < orders_size;j++){
-
-            if(strcmp(categories[i].name, orders[j].pizza_category) == 0){
-                categories[i].quantity = categories[i].quantity + orders[j].quantity;
-
-            }
-
-        }
-    }
-
-}
-
-void categories_fill(Order *orders, int orders_size, int categories_size, Category *categories){
-
-    int index = 0;
-    
-    char explored[categories_size][20];
-    // Llenar los nombres de las categorías
-    for (int i = 0; i < orders_size; i++) {
-        int is_explored = 0;
-
-        // Verificar si la categoría ya ha sido registrada
-        for (int j = 0; j < categories_size; j++) {
-            if (strcmp(orders[i].pizza_category, explored[j]) == 0) {
-                is_explored = 1;
-                break;
-            }
-        }
-
-        // Si la categoría es nueva, agregarla a explored
-        if (is_explored == 0) {
-            strcpy(explored[index], orders[i].pizza_category);
-            index++;
-        }
-    }
-    // Agregar las categorias de explored a las estructuras
-    for (int i = 0; i < categories_size; i++){
-        strcpy(categories[i].name, explored[i]);
-        categories[i].quantity = 0; // Inicializar quantity 
-
-    }
-
-
-}
-
-
-
-
+// Calcular cantidad de categorias unicas de pizza
 int unique_categories(Order *orders, int size) {
     char explored[size][20];
     int index = 0;
@@ -86,6 +35,57 @@ int unique_categories(Order *orders, int size) {
     return index;  // Retorna el número de categorías únicas
 }
 
+// Ingresa los nombres de las categorias a los struct
+void categories_fill(Order *orders, int orders_size, int categories_size, Category *categories){
+
+    int index = 0;
+    
+    char explored[categories_size][20];
+    // Llenar los nombres de las categorías
+    for (int i = 0; i < orders_size; i++) {
+        int is_explored = 0;
+
+        // Verificar si la categoría ya ha sido registrada
+        for (int j = 0; j < categories_size; j++) {
+            if (strcmp(orders[i].pizza_category, explored[j]) == 0) {
+                is_explored = 1;
+                break;
+            }
+        }
+
+        // Si la categoría es nueva agregarla al struct y agregarla a explored
+        if (is_explored == 0) {
+            strcpy(explored[index], orders[i].pizza_category);
+            index++;
+        }
+    }
+    // Agregar las categorias de explored a las estructuras
+    for (int i = 0; i < categories_size; i++){
+        strcpy(categories[i].name, explored[i]);
+        categories[i].quantity = 0; // Inicializar quantity 
+
+    }
+}
+
+// Ingresa cantidad de pizzas por categoria a struct
+void quantities_fill(Order *orders, int orders_size, int categories_size, Category *categories){
+    for(int i = 0; i < categories_size; i++){
+        for(int j = 0; j < orders_size;j++){
+
+            if(strcmp(categories[i].name, orders[j].pizza_category) == 0){
+                categories[i].quantity = categories[i].quantity + orders[j].quantity;
+
+            }
+
+        }
+    }
+
+}
+
+
+
+
+// Ventas por categoria
 void hp(Order *orders, int index){
     // Obtener cantidad de categorias unicas
     int categories_size = unique_categories(orders,index);
